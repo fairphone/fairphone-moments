c78408bac24486a917bd87670972180c2ac1d166
Eduardo Pinar Menoyo <eduardo@fairphone.com>
2026-02-12 17:16:09 +0100

    Refactor: Use Protobuf builder for profile conversion

--- app/src/main/java/com/fairphone/spring/launcher/data/model/CreateLauncherProfile.kt
+++ app/src/main/java/com/fairphone/spring/launcher/data/model/CreateLauncherProfile.kt
@@ -9,10 +9,10 @@
 package com.fairphone.spring.launcher.data.model
 
 import com.fairphone.spring.launcher.data.model.protos.ContactType
+import com.fairphone.spring.launcher.data.model.protos.LauncherProfile
 import com.fairphone.spring.launcher.data.model.protos.LauncherProfileApp
 import com.fairphone.spring.launcher.data.model.protos.SoundSetting
 import com.fairphone.spring.launcher.data.model.protos.UiMode
-import com.fairphone.spring.launcher.data.model.protos.launcherProfile
 
 /**
  * Data Class used to build a new LauncherProfile.
@@ -36,22 +36,24 @@ data class CreateLauncherProfile(
     val reduceBrightnessEnabled: Boolean,
 )
 
-fun CreateLauncherProfile.toLauncherProfile() = launcherProfile {
-    id = this@toLauncherProfile.id
-    name = this@toLauncherProfile.name
-    icon = this@toLauncherProfile.icon
-    bgColor1 = this@toLauncherProfile.bgColor1
-    bgColor2 = this@toLauncherProfile.bgColor2
-    launcherProfileApps.addAll(this@toLauncherProfile.launcherProfileApps)
-    allowedContacts = this@toLauncherProfile.allowedContacts
-    customContacts.addAll(this@toLauncherProfile.customContacts)
-    repeatCallEnabled = this@toLauncherProfile.repeatCallEnabled
-    wallpaperId = this@toLauncherProfile.wallpaperId
-    uiMode = this@toLauncherProfile.uiMode
-    blueLightFilterEnabled = this@toLauncherProfile.blueLightFilterEnabled
-    grayScaleEnabled = this@toLauncherProfile.grayScaleEnabled
-    soundSetting = this@toLauncherProfile.soundSetting
-    batterySaverEnabled = this@toLauncherProfile.batterySaverEnabled
-    reduceBrightnessEnabled = this@toLauncherProfile.reduceBrightnessEnabled
-    zenRuleId = ""
-}
+/**
+ * Extension function to convert a [CreateLauncherProfile] to a [LauncherProfile].
+ */
+fun CreateLauncherProfile.toLauncherProfile(): LauncherProfile = LauncherProfile.newBuilder()
+    .setId(id)
+    .setName(name)
+    .setIcon(icon)
+    .setBgColor1(bgColor1)
+    .setBgColor2(bgColor2)
+    .addAllLauncherProfileApps(launcherProfileApps)
+    .setAllowedContacts(allowedContacts)
+    .addAllCustomContacts(customContacts)
+    .setRepeatCallEnabled(repeatCallEnabled)
+    .setWallpaperId(wallpaperId)
+    .setUiMode(uiMode)
+    .setBlueLightFilterEnabled(blueLightFilterEnabled)
+    .setGrayScaleEnabled(grayScaleEnabled)
+    .setSoundSetting(soundSetting)
+    .setBatterySaverEnabled(batterySaverEnabled)
+    .setReduceBrightnessEnabled(reduceBrightnessEnabled)
+    .build()
