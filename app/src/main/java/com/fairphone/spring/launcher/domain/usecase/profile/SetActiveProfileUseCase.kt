2abc01fb2c710ce16040d4c086b16399661407a9
Eduardo Pinar Menoyo <eduardo@fairphone.com>
2026-02-12 17:17:34 +0100

    Refactor: Use EnableDndUseCase in SetActiveProfileUseCase

--- app/src/main/java/com/fairphone/spring/launcher/domain/usecase/profile/SetActiveProfileUseCase.kt
+++ app/src/main/java/com/fairphone/spring/launcher/domain/usecase/profile/SetActiveProfileUseCase.kt
@@ -10,20 +10,20 @@ package com.fairphone.spring.launcher.domain.usecase.profile
 
 import com.fairphone.spring.launcher.data.model.protos.LauncherProfile
 import com.fairphone.spring.launcher.data.repository.LauncherProfileRepository
-import com.fairphone.spring.launcher.domain.usecase.ToggleDndUseCase
+import com.fairphone.spring.launcher.domain.usecase.EnableDndUseCase
 import com.fairphone.spring.launcher.domain.usecase.base.UseCase
 import kotlinx.coroutines.flow.first
 
 class SetActiveProfileUseCase(
     private val launcherProfileRepository: LauncherProfileRepository,
-    private val toggleDnDUseCase: ToggleDndUseCase,
+    private val enableDnDUseCase: EnableDndUseCase,
 ) : UseCase<String, LauncherProfile>() {
     override suspend fun execute(params: String): Result<LauncherProfile> {
         return try {
-            toggleDnDUseCase.execute(false)
+            enableDnDUseCase.execute(false)
             val newActiveProfile = launcherProfileRepository.getProfile(params).first()
             launcherProfileRepository.setActiveProfile(params)
-            toggleDnDUseCase.execute(true)
+            enableDnDUseCase.execute(true)
             return Result.success(newActiveProfile)
         } catch (e: Exception) {
             Result.failure(e)
