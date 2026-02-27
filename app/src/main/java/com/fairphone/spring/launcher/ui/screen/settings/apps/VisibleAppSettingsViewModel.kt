33e130a899ae5d20227fd1ca32b7b18012929413
Eduardo Pinar Menoyo <eduardo@fairphone.com>
2026-02-27 17:59:20 +0100

    Fix VisibleAppSettings not reloading when updating them

--- app/src/main/java/com/fairphone/spring/launcher/ui/screen/settings/apps/VisibleAppSettingsViewModel.kt
+++ app/src/main/java/com/fairphone/spring/launcher/ui/screen/settings/apps/VisibleAppSettingsViewModel.kt
@@ -8,22 +8,6 @@
 
 package com.fairphone.spring.launcher.ui.screen.settings.apps
 
-/*
- * Copyright (c) 2025. FairPhone B.V.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -35,10 +19,10 @@ import com.fairphone.spring.launcher.data.repository.AppInfoRepository
 import com.fairphone.spring.launcher.domain.usecase.profile.GetEditedProfileUseCase
 import com.fairphone.spring.launcher.domain.usecase.profile.UpdateLauncherProfileUseCase
 import com.fairphone.spring.launcher.util.permute
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class VisibleAppSettingsViewModel(
@@ -48,22 +32,20 @@ class VisibleAppSettingsViewModel(
     private val updateLauncherProfileUseCase: UpdateLauncherProfileUseCase,
 ) : ViewModel() {
 
-    private val _screenState: MutableStateFlow<VisibleAppSettingsScreenState> =
-        MutableStateFlow(VisibleAppSettingsScreenState.Loading)
-    val screenState = _screenState.asStateFlow()
     lateinit var profile: LauncherProfile
 
-    init {
-        viewModelScope.launch {
-            _screenState.update {
-                profile = getEditedProfileUseCase.execute(Unit).first()
-                val visibleApps = getAppInfoList(context, profile.launcherProfileAppsList)
-                VisibleAppSettingsScreenState.Ready(
-                    visibleApps = visibleApps,
-                )
-            }
-        }
-    }
+    val screenState: StateFlow<VisibleAppSettingsScreenState> =
+        getEditedProfileUseCase.execute(Unit).map { profile ->
+            this.profile = profile
+            val visibleApps = getAppInfoList(context, profile.launcherProfileAppsList)
+            VisibleAppSettingsScreenState.Ready(
+                visibleApps = visibleApps,
+            )
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = VisibleAppSettingsScreenState.Loading,
+        )
 
     private fun getAppInfoList(context: Context, appIds: List<LauncherProfileApp>): List<AppInfo> {
         return appInfoRepository.getAppInfosByProfileApps(context, appIds)
@@ -74,28 +56,19 @@ class VisibleAppSettingsViewModel(
             if(currentIndex == targetIndex) {
                 return@launch
             } else {
-                _screenState.update { state ->
-                    when (state) {
-                        is VisibleAppSettingsScreenState.Loading -> state
-                        is VisibleAppSettingsScreenState.Ready -> {
-                            val updatedApps = state.visibleApps.permute(currentIndex, targetIndex)
-                            val launcherProfileApps = updatedApps.map { it.toLauncherProfileApp() }
-                            profile = profile
-                                .toBuilder()
-                                .clearLauncherProfileApps()
-                                .addAllLauncherProfileApps(launcherProfileApps)
-                                .build()
-                            val  result = updateLauncherProfileUseCase.execute(profile)
-                            if(result.isSuccess) {
-                                state.copy(visibleApps = updatedApps)
-                            } else {
-                                state
-                            }
-                        }
+                screenState.value.let { state ->
+                    if (state is VisibleAppSettingsScreenState.Ready) {
+                        val updatedApps = state.visibleApps.permute(currentIndex, targetIndex)
+                        val launcherProfileApps = updatedApps.map { it.toLauncherProfileApp() }
+                        profile = profile
+                            .toBuilder()
+                            .clearLauncherProfileApps()
+                            .addAllLauncherProfileApps(launcherProfileApps)
+                            .build()
+                        updateLauncherProfileUseCase.execute(profile)
                     }
                 }
             }
-
         }
     }
 }
